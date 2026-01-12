1aa37d7008018da3b55334874e533a86203632f1
Fred Feraco <frederick.feraco@gmail.com>
2026-01-12 08:53:20 -0500

    Updated arduinoBlocks.js

--- src/lib/arduinoBlocks.js
+++ src/lib/arduinoBlocks.js
@@ -73,7 +73,7 @@ export function defineArduinoBlocks() {
     }
   };
 
-  Blockly.Blocks['arduino_stop'] = {
+  Blockly.Blocks['arduino_stop_motors'] = {
     init: function() {
       this.appendDummyInput()
           .appendField('stop motors');
@@ -85,44 +85,283 @@ export function defineArduinoBlocks() {
     }
   };
 
-  Blockly.Blocks['arduino_delay'] = {
+  Blockly.Blocks['arduino_get_distance'] = {
     init: function() {
+      this.appendDummyInput()
+          .appendField('get distance (cm)');
+      this.setOutput(true, 'Number');
+      this.setColour(160);
+      this.setTooltip('Get distance from ultrasonic sensor in cm');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['arduino_follow_line'] = {
+    init: function() {
+      this.appendValueInput('threshold')
+          .setCheck('Number')
+          .appendField('follow line with threshold');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(160);
+      this.setTooltip('Follow a line using line sensors');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['arduino_look_left'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('look left');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(260);
+      this.setTooltip('Turn servo to look left');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['arduino_look_right'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('look right');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(260);
+      this.setTooltip('Turn servo to look right');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['arduino_look_center'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('look center');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(260);
+      this.setTooltip('Center the servo');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['arduino_open_claw'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('open claw');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(290);
+      this.setTooltip('Open the claw');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['arduino_close_claw'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('close claw');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(290);
+      this.setTooltip('Close the claw');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['arduino_beep'] = {
+    init: function() {
+      this.appendValueInput('frequency')
+          .setCheck('Number')
+          .appendField('beep frequency');
       this.appendValueInput('duration')
           .setCheck('Number')
-          .appendField('wait');
+          .appendField('duration');
       this.appendDummyInput()
           .appendField('ms');
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
-      this.setColour(120);
-      this.setTooltip('Wait for specified milliseconds');
+      this.setColour(160);
+      this.setTooltip('Play a beep sound');
       this.setHelpUrl('');
     }
   };
 
-  Blockly.Blocks['arduino_read_ultrasonic'] = {
+  Blockly.Blocks['arduino_light_rgb'] = {
     init: function() {
-      this.appendDummyInput()
-          .appendField('read ultrasonic sensor (cm)');
-      this.setOutput(true, 'Number');
-      this.setColour(260);
-      this.setTooltip('Read distance from ultrasonic sensor');
+      this.appendValueInput('red')
+          .setCheck('Number')
+          .appendField('set RGB LED R');
+      this.appendValueInput('green')
+          .setCheck('Number')
+          .appendField('G');
+      this.appendValueInput('blue')
+          .setCheck('Number')
+          .appendField('B');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(160);
+      this.setTooltip('Set RGB LED color');
       this.setHelpUrl('');
     }
   };
 
-  Blockly.Blocks['arduino_read_line_sensor'] = {
+  Blockly.Blocks['arduino_light_off'] = {
     init: function() {
       this.appendDummyInput()
-          .appendField('read line sensor')
+          .appendField('turn LED off');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(160);
+      this.setTooltip('Turn off the LED');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['plotter_init'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('initialize plotter');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(20);
+      this.setTooltip('Initialize data plotter');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['plotter_showHide'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('toggle plotter visibility');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(20);
+      this.setTooltip('Show or hide the plotter');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['plotter_clear'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('clear plotter');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(20);
+      this.setTooltip('Clear the plotter');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['plotter_drawGrid'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('draw grid');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(20);
+      this.setTooltip('Draw grid on plotter');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['plotter_setColor'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField('set plotter color');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(20);
+      this.setTooltip('Set plotter color');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['plotter_setPointSize'] = {
+    init: function() {
+      this.appendValueInput('size')
+          .setCheck('Number')
+          .appendField('set point size');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(20);
+      this.setTooltip('Set point size for plotter');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['plotter_drawPoint'] = {
+    init: function() {
+      this.appendValueInput('x')
+          .setCheck('Number')
+          .appendField('draw point at x');
+      this.appendValueInput('y')
+          .setCheck('Number')
+          .appendField('y');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(20);
+      this.setTooltip('Draw a point on the plotter');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['plotter_drawLine'] = {
+    init: function() {
+      this.appendValueInput('x1')
+          .setCheck('Number')
+          .appendField('draw line from x1');
+      this.appendValueInput('y1')
+          .setCheck('Number')
+          .appendField('y1');
+      this.appendValueInput('x2')
+          .setCheck('Number')
+          .appendField('to x2');
+      this.appendValueInput('y2')
+          .setCheck('Number')
+          .appendField('y2');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(20);
+      this.setTooltip('Draw a line on the plotter');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['plotter_drawTriangle'] = {
+    init: function() {
+      this.appendValueInput('x')
+          .setCheck('Number')
+          .appendField('draw triangle at x');
+      this.appendValueInput('y')
+          .setCheck('Number')
+          .appendField('y');
+      this.appendValueInput('dir')
+          .setCheck('Number')
+          .appendField('direction');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(20);
+      this.setTooltip('Draw a triangle on the plotter');
+      this.setHelpUrl('');
+    }
+  };
+
+  Blockly.Blocks['sleep'] = {
+    init: function() {
+      this.appendValueInput('seconds')
+          .setCheck('Number')
+          .appendField('wait');
+      this.appendDummyInput()
           .appendField(new Blockly.FieldDropdown([
-            ['left', 'LEFT'],
-            ['center', 'CENTER'],
-            ['right', 'RIGHT']
-          ]), 'position');
-      this.setOutput(true, 'Boolean');
-      this.setColour(260);
-      this.setTooltip('Read line sensor (returns true if line detected)');
+            ['seconds', 'SECONDS'],
+            ['milliseconds', 'MILLISECONDS']
+          ]), 'units');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(120);
+      this.setTooltip('Wait for specified time');
       this.setHelpUrl('');
     }
   };
@@ -130,6 +369,36 @@ export function defineArduinoBlocks() {
 
 export function defineArduinoGenerators() {
   Blockly.Arduino = Blockly.Arduino || {};
+
+  Blockly.Arduino.ORDER_ATOMIC = 0;
+  Blockly.Arduino.ORDER_MEMBER = 1;
+  Blockly.Arduino.ORDER_FUNCTION_CALL = 2;
+  Blockly.Arduino.ORDER_UNARY_POSTFIX = 3;
+  Blockly.Arduino.ORDER_UNARY_PREFIX = 4;
+  Blockly.Arduino.ORDER_MULTIPLICATIVE = 5;
+  Blockly.Arduino.ORDER_ADDITIVE = 6;
+  Blockly.Arduino.ORDER_SHIFT = 7;
+  Blockly.Arduino.ORDER_RELATIONAL = 8;
+  Blockly.Arduino.ORDER_EQUALITY = 9;
+  Blockly.Arduino.ORDER_BITWISE_AND = 10;
+  Blockly.Arduino.ORDER_BITWISE_XOR = 11;
+  Blockly.Arduino.ORDER_BITWISE_OR = 12;
+  Blockly.Arduino.ORDER_LOGICAL_AND = 13;
+  Blockly.Arduino.ORDER_LOGICAL_OR = 14;
+  Blockly.Arduino.ORDER_CONDITIONAL = 15;
+  Blockly.Arduino.ORDER_ASSIGNMENT = 16;
+  Blockly.Arduino.ORDER_NONE = 99;
+
+  Blockly.Arduino.valueToCode = function(block, name, order) {
+    if (order === undefined) {
+      order = Blockly.Arduino.ORDER_NONE;
+    }
+    return Blockly.Generator.prototype.valueToCode.call(this, block, name, order);
+  };
+
+  Blockly.Arduino.blockToCode = function(block) {
+    return Blockly.Generator.prototype.blockToCode.call(this, block);
+  };
 
   Blockly.Arduino['arduino_move_forward'] = function(block) {
     const speed = Blockly.Arduino.valueToCode(block, 'speed', Blockly.Arduino.ORDER_ATOMIC) || '255';
@@ -155,21 +424,109 @@ export function defineArduinoGenerators() {
     return `turnRight(${speed}, ${duration});\n`;
   };
 
-  Blockly.Arduino['arduino_stop'] = function(block) {
+  Blockly.Arduino['arduino_stop_motors'] = function(block) {
     return 'stopMotors();\n';
   };
 
-  Blockly.Arduino['arduino_delay'] = function(block) {
-    const duration = Blockly.Arduino.valueToCode(block, 'duration', Blockly.Arduino.ORDER_ATOMIC) || '1000';
-    return `delay(${duration});\n`;
+  Blockly.Arduino['arduino_get_distance'] = function(block) {
+    return ['getDistance()', Blockly.Arduino.ORDER_ATOMIC];
   };
 
-  Blockly.Arduino['arduino_read_ultrasonic'] = function(block) {
-    return ['readUltrasonic()', Blockly.Arduino.ORDER_ATOMIC];
+  Blockly.Arduino['arduino_follow_line'] = function(block) {
+    const threshold = Blockly.Arduino.valueToCode(block, 'threshold', Blockly.Arduino.ORDER_ATOMIC) || '500';
+    return `followLine(${threshold});\n`;
   };
 
-  Blockly.Arduino['arduino_read_line_sensor'] = function(block) {
-    const position = block.getFieldValue('position');
-    return [`readLineSensor(${position})`, Blockly.Arduino.ORDER_ATOMIC];
+  Blockly.Arduino['arduino_look_left'] = function(block) {
+    return 'lookLeft();\n';
+  };
+
+  Blockly.Arduino['arduino_look_right'] = function(block) {
+    return 'lookRight();\n';
+  };
+
+  Blockly.Arduino['arduino_look_center'] = function(block) {
+    return 'lookCenter();\n';
+  };
+
+  Blockly.Arduino['arduino_open_claw'] = function(block) {
+    return 'openClaw();\n';
+  };
+
+  Blockly.Arduino['arduino_close_claw'] = function(block) {
+    return 'closeClaw();\n';
+  };
+
+  Blockly.Arduino['arduino_beep'] = function(block) {
+    const frequency = Blockly.Arduino.valueToCode(block, 'frequency', Blockly.Arduino.ORDER_ATOMIC) || '1000';
+    const duration = Blockly.Arduino.valueToCode(block, 'duration', Blockly.Arduino.ORDER_ATOMIC) || '200';
+    return `tone(BUZZER_PIN, ${frequency}, ${duration});\n`;
+  };
+
+  Blockly.Arduino['arduino_light_rgb'] = function(block) {
+    const red = Blockly.Arduino.valueToCode(block, 'red', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    const green = Blockly.Arduino.valueToCode(block, 'green', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    const blue = Blockly.Arduino.valueToCode(block, 'blue', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    return `setRGB(${red}, ${green}, ${blue});\n`;
+  };
+
+  Blockly.Arduino['arduino_light_off'] = function(block) {
+    return 'setRGB(0, 0, 0);\n';
+  };
+
+  Blockly.Arduino['plotter_init'] = function(block) {
+    return '// Plotter initialized\n';
+  };
+
+  Blockly.Arduino['plotter_showHide'] = function(block) {
+    return '// Toggle plotter\n';
+  };
+
+  Blockly.Arduino['plotter_clear'] = function(block) {
+    return '// Clear plotter\n';
+  };
+
+  Blockly.Arduino['plotter_drawGrid'] = function(block) {
+    return '// Draw grid\n';
+  };
+
+  Blockly.Arduino['plotter_setColor'] = function(block) {
+    return '// Set color\n';
+  };
+
+  Blockly.Arduino['plotter_setPointSize'] = function(block) {
+    const size = Blockly.Arduino.valueToCode(block, 'size', Blockly.Arduino.ORDER_ATOMIC) || '2';
+    return `// Set point size: ${size}\n`;
+  };
+
+  Blockly.Arduino['plotter_drawPoint'] = function(block) {
+    const x = Blockly.Arduino.valueToCode(block, 'x', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    const y = Blockly.Arduino.valueToCode(block, 'y', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    return `// Draw point at (${x}, ${y})\n`;
+  };
+
+  Blockly.Arduino['plotter_drawLine'] = function(block) {
+    const x1 = Blockly.Arduino.valueToCode(block, 'x1', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    const y1 = Blockly.Arduino.valueToCode(block, 'y1', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    const x2 = Blockly.Arduino.valueToCode(block, 'x2', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    const y2 = Blockly.Arduino.valueToCode(block, 'y2', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    return `// Draw line from (${x1}, ${y1}) to (${x2}, ${y2})\n`;
+  };
+
+  Blockly.Arduino['plotter_drawTriangle'] = function(block) {
+    const x = Blockly.Arduino.valueToCode(block, 'x', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    const y = Blockly.Arduino.valueToCode(block, 'y', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    const dir = Blockly.Arduino.valueToCode(block, 'dir', Blockly.Arduino.ORDER_ATOMIC) || '0';
+    return `// Draw triangle at (${x}, ${y}) dir ${dir}\n`;
+  };
+
+  Blockly.Arduino['sleep'] = function(block) {
+    const value = Blockly.Arduino.valueToCode(block, 'seconds', Blockly.Arduino.ORDER_ATOMIC) || '1';
+    const units = block.getFieldValue('units');
+    if (units === 'SECONDS') {
+      return `delay(${value} * 1000);\n`;
+    } else {
+      return `delay(${value});\n`;
+    }
   };
 }
